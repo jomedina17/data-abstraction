185df87a8f4ac9c18a34349d584a354566a1778b
Jocelyn Medina <113551944+jomedina17@users.noreply.github.com>
2023-02-22 20:59:06 +0000

    titles

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # make sure you fork this and git repo it
 # go to the shell command screen and type
-# pip install -r requirements.txt
+#
+#pip install -r requirements.txt
 #to install the necessary components
 import requests
 from bs4 import BeautifulSoup
@@ -11,11 +12,25 @@ headers = {
   'user-agent':
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
 }
+page = requests.get(url, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+#get all books
+books = soup.find_all(id="gridItemRoot")
+book = books[0]
+rank = book.find('span', class_='zg-bdg-text').text[1:]
+print(rank)
+
+children = book.find('div', class_='zg-grid-general-faceout').div
+children.contents[0]
+
+title = children.contents[1]
+print(title)
 
 page = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
+book = books[0]
+#soup = BeautifulSoup(page.content, 'html.parser')
+#print(soup.prettify())
 #get all books
 
 #get info from first book
